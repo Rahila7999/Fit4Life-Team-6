129b73a4b5795ce69ff2b0ee26a8e4034e01ae13
Gaurav-Ji145 <gs1868022@gmail.com>
2024-08-20 16:05:46 +0530

    connect all codes with eclipse

---
+++ Frontend/src/test/java/com/adit/Gaurav/GauravApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adit.Gaurav;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GauravApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
